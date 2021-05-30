9c6762c4158740e8473e306971f0cd7239cc5ade
mcaceresDev <mjcaceres17@gmail.com>
2021-05-30 12:30:10 -0600

    Edicion y Eliminacion - (Busqueda Pendiente)

--- js/main.js
+++ js/main.js
@@ -9,12 +9,13 @@ const db = window.localStorage;
 //Menu de pestañas
 const despliegaPestana = (e) =>{
     e.preventDefault();
+    
     let objetivo = e.target.getAttribute("href");document.querySelectorAll(".contenido-pestanas .bloque")
     .forEach( (elemento) => {
       elemento.classList.remove("activo");
     });
     document.querySelector(objetivo).classList.add("activo");
-
+    
     //-------------------------------------------------------
 
     let pestana = e.target.parentNode;
@@ -32,29 +33,27 @@ document.querySelectorAll(".navegacion li a").forEach( (elemento) => {
 
 
 //****************************** */
-let Id;
-let historial = [];
+
+var historial = [];
 //***************************** */
 const recogeDatos = (e) => {
     e.preventDefault();
+
     let valida = false;
     
     do {
-        Id = Math.round(Math.random() * (5 - 1) + 1);
+        let Id = Math.round(Math.random() * (5 - 1) + 1);
+
         if (historial.includes(Id) && historial.length < 5) {
             valida = false;
         }       
         else if(historial.length < 5 ) {
+            
             historial.push(Id);
+            guardarContacto(db, Id);
+
             valida = true;
             
-            document.querySelector("#md1").classList.add("visible")
-            setTimeout(function(){
-                document.querySelector("#md1").classList.remove("visible");
-            },2000)
-            document.querySelector("#formulario").reset();
-            
-            guardarContacto(db, Id);
         }
         else{
             valida = true;
@@ -68,6 +67,9 @@ const recogeDatos = (e) => {
     } while (valida === false);
     
 }
+
+//---- Agregar Contactos (Guardarlos)
+
 const btnAgrega = document.querySelector("#btnAgrega");
 btnAgrega.addEventListener("click", recogeDatos);
 
@@ -80,133 +82,157 @@ const guardarContacto = (db, Id) => {
             numero: numero.value,
             correo: correo.value
         }
-    db.setItem(contacto.id, JSON.stringify(contacto));
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const btnAgrega99 = document.querySelector("#btnAgrega");
-// btnAgrega99.addEventListener("click", guardaDatos = ()  => {  
-//     e.preventDefault();
-//     let contacto = {
-//         id: contadorId,
-//         nombre: nombre.value,
-//         apellido: apellido.value,
-//         numero: numero.value,
-//         correo: correo.value
-//     }
-//     guardarContacto(db, contacto);
-//     agregaFila(contacto);
-//     contadorId++;
-//     document.getElementById("formulario").reset();
-// });
-
-
-
-
-const cargarContactos = (db) => {
-    let claves = Object.keys(db);
-    // console.log(claves);
-    for(clave of claves){
-        let contacto = JSON.parse(db.getItem(clave));
-        // console.log(contacto.length);
-        agregaFila(contacto);
-
-    //     let item = document.createElement("TR");
-
-    // let idContacto = document.createElement("TD")
-    // let nombreContacto = document.createElement("TD")
-    // let numeroContacto = document.createElement("TD")
-    // let correoContacto = document.createElement("TD")
-    // let edita = document.createElement("TD");
-    // let elimina = document.createElement("TD");
-
-    // idContacto.innerHTML = contacto.id;
-    // nombreContacto.innerHTML = `${contacto.nombre} ${contacto.apellido}`;
-    // numeroContacto.innerHTML = contacto.numero;
-    // correoContacto.innerHTML = contacto.correo;
-    // edita.innerHTML = `<button class='btn-edit'><span class="material-icons">edit</span></button>`;
-    // elimina.innerHTML = `<button class='btn-danger'><span class="material-icons">delete_outline</span></button>`;
-
-    // item.appendChild(idContacto);
-    // item.appendChild(nombreContacto);
-    // item.appendChild(numeroContacto);
-    // item.appendChild(correoContacto);
-    // item.appendChild(edita);
-    // item.appendChild(elimina);
     
+    let verifica = verificaContacto(contacto);
 
-    // lista.appendChild(item);
+    if (verifica === true) {
+        let mdError =document.querySelector("#md2");
+        
+        document.querySelector("#md2 .texto").innerHTML = "<h2>El contacto ya existe en tu lista</h2>" + 
+        "<p>El número o correo ya están asociados a un contacto. Verifica y reintenta</p>";
+        mdError.classList.add("visible")
+        
+        setTimeout(function(){
+            document.querySelector("#md2").classList.remove("visible");
+        },2000)
+        
+        return;
+    }
+    else{
+        db.setItem(contacto.id, JSON.stringify(contacto));
+        
+        document.querySelector("#md1").classList.add("visible")
+            setTimeout(function(){
+                document.querySelector("#md1").classList.remove("visible");
+            },2000)
+            document.querySelector("#formulario").reset();
 
+        cargarContactos(db);
     }
 }
 
- 
-const agregaFila = (contacto) => {
-    let item = document.createElement("TR");
-
-    let idContacto = document.createElement("TD")
-    let nombreContacto = document.createElement("TD")
-    let numeroContacto = document.createElement("TD")
-    let correoContacto = document.createElement("TD")
-    let edita = document.createElement("TD");
-    let elimina = document.createElement("TD");
-
-    idContacto.innerHTML = contacto.id;
-    nombreContacto.innerHTML = `${contacto.nombre} ${contacto.apellido}`;
-    numeroContacto.innerHTML = contacto.numero;
-    correoContacto.innerHTML = contacto.correo;
-    edita.innerHTML = `<button class='btn-edit material-icons'>edit</button>`;
-    elimina.innerHTML = `<button class='btn-danger'><span class="material-icons">delete_outline</span></button>`;
-
-    item.appendChild(idContacto);
-    item.appendChild(nombreContacto);
-    item.appendChild(numeroContacto);
-    item.appendChild(correoContacto);
-    item.appendChild(edita);
-    item.appendChild(elimina);
+//Validar si existe contacto
+function verificaContacto (valores){
+    let claves = Object.keys(db);
     
-
-    lista.appendChild(item);
-};
-
-
-// const editar = () => {
-
-// };
-
-
-
-const editar = (e) => {
-    let nodoTD = e.target.parentNode;
-    let nodoTR = nodoTD.parentNode;
-    let items = nodoTR.querySelectorAll("td");
-    let datos = Array.from(items);
-
-    nombre.value = datos[1].textContent; 
-    numero.value = datos[2].textContent;
-    correo.value = datos[3].textContent;
-
-    console.log(datos[1].textContent);
-    // nodoTR.querySelectorAll("td").forEach(function(elemento){
-    //     console.log(elemento.textContent); 
-    // });
+    for(clave of claves){
+        let contacto = JSON.parse(db.getItem(clave));
+        
+        if (contacto.numero == valores.numero || contacto.correo === valores.correo) {
+               
+            historial.pop();
+            return true;
+        }
+        else{
+            return false;
+        }
+        
+    }
 }
 
-// var btnEdita = document.querySelector("button.btn-edit");
-// btnEdita.addEventListener("click", editar);
+// Generar Listado de Contactos Guardados
+const cargarContactos =  (db) => {
+    let claves = Object.keys(db);
+    lista.innerHTML = " ";
+    
+    for(clave of claves){
+        let contacto = JSON.parse(db.getItem(clave));
+        
+        let item = document.createElement("ul");
+        item.classList.add("contacto");
+        item.setAttribute("data-ident", contacto.id);
+
+        item.innerHTML = `<li>${contacto.nombre} ${contacto.apellido} 
+        <span> 
+               <button class="material-icons btnEdita">edit</button> 
+               <button class="material-icons btnElimina">delete_outline</button> 
+        </span> </li>
+        <li>${contacto.numero}</li>
+        <li>${contacto.correo}</li>`; 
+        lista.appendChild(item);
+        
+    }
+
+    document.querySelectorAll(".contacto").forEach(function(contact){
+        contact.addEventListener("click", function() {
+            this.classList.toggle("completo")
+        })
+    });
+    document.querySelectorAll(".btnEdita").forEach(function(elemento){
+        elemento.addEventListener("click", editarContacto);
+    });
+    document.querySelectorAll(".btnElimina").forEach(function(elemento){
+        elemento.addEventListener("click", eliminarContacto);
+    });
+}
+
+cargarContactos(db);
+ 
+
+function eliminarContacto(e){
+    e.stopPropagation();
+    let btnGroup = this.parentNode;
+    let li = btnGroup.parentNode;
+    let contacto = li.parentNode.dataset.ident;
+        
+    let modalElim = document.querySelector("#md3");
+    modalElim.classList.add("visible");
+    let btnBorrar = document.querySelector("#md3  .btnBorrar").addEventListener("click", () => {
+        db.removeItem(contacto);
+        historial.splice(0, contacto - 1);
+        cargarContactos(db);
+        modalElim.classList.remove("visible");
+    }); 
+    let btnCancelar = document.querySelector("#md3 .btnCancelar").addEventListener("click", () => modalElim.classList.remove("visible"));
+
+}
 
 
+function editarContacto(e){
+    e.stopPropagation();
+
+    let btnGroup = this.parentNode;
+    let li = btnGroup.parentNode;
+    let id = parseInt(li.parentNode.dataset.ident);
+    
+    let contacto = JSON.parse(db.getItem(id));
+    
+    let Nombres = document.querySelector(".frmEdicion .nombreC");
+    let Apellido = document.querySelector(".frmEdicion .apellidoC");
+    let Numero = document.querySelector(".frmEdicion .numeroC");
+    let Email = document.querySelector(".frmEdicion .emailC");
+
+    Nombres.value = contacto.nombre;
+    Apellido.value = contacto.apellido;
+    Numero.value = contacto.numero;
+    Email.value = contacto.correo;
+
+    document.querySelector("#md4").classList.add("visible");    
+    edita(db, id)  
+}
+
+
+const edita = (db, Id) => {
+    let Nombres = document.querySelector(".frmEdicion .nombreC");
+    let Apellido = document.querySelector(".frmEdicion .apellidoC");
+    let Numero = document.querySelector(".frmEdicion .numeroC");
+    let Email = document.querySelector(".frmEdicion .emailC");
+
+    
+    document.querySelector("#reemplaza").addEventListener("click", function(e){
+            e.preventDefault();
+
+            let contacto = {
+                id: Id,
+                nombre: Nombres.value,
+                apellido: Apellido.value,
+                numero: Numero.value,
+                correo: Email.value
+            }
+
+            db.setItem(contacto.id, JSON.stringify(contacto));
+            document.querySelector("#md4").classList.remove("visible");
+            cargarContactos(db);
+    });
+}
 
